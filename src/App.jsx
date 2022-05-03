6c08bb826f964758c003fbbe9bb377a8b17aeda6
petya <sashafem@mail.ru>
2022-05-03 12:25:08 +0300

    First

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Layout from './components/common/Layout';
+import Home from './components/pages/Home/Home';
+
+const App = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  )
+}
+
+export default App
